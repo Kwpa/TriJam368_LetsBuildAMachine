a30a6a1312563351eba96eccef325b1b3d105fc5
Kwpa <kai.mcgilligan.oliver@gmail.com>
2026-04-18 13:50:45 -0300

    [Kai] Setup for godot structure

---
+++ scripts/test_script.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+@export var color : Color = Color.BURLYWOOD
+var statement = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	statement = 10
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
