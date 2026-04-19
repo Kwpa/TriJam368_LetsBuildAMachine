551ff418f7b94c2983f563712bd832e31128229b
Kwpa <kai.mcgilligan.oliver@gmail.com>
2026-04-18 23:04:06 -0300

    [Kai] Added plant behaviour

--- scripts/plant.gd
+++ scripts/plant.gd
@@ -12,12 +12,46 @@ var plant_statisfied_round_count : int = 0
 
 var warning_queue : Array = []
 
+var resource_inputs : Array[String] = []
+
 func _ready():
-	print("hook up events")
+	## sigal for input growth
+	print("hook up signals")
 
+## to hook up to signal for when a tile is placed or moved, and we need to add or remove resources going into the plant 
+func resource_input(input_name:String, action_type: String):
+	match action_type:
+		"remove": 
+			if resource_inputs.has(input_name):
+				var index = resource_inputs.find(input_name)
+				resource_inputs.remove_at(index)
+		"add":
+			resource_inputs.append(input_name)
 
+## check the resource inputs to see if there enough to make the plant grow 
+func check_inputs_for_growth() -> bool:
+	var photosyn_count = 0
+	var moisture_count = 0
+	var nutrients_count = 0
+	for input : String in resource_inputs:
+		match input:
+			"light":
+				photosyn_count += 1
+			"water":
+				moisture_count += 1
+			"fertilizer":
+				nutrients_count += 1
+	
+	## show icons signalling inputs contributing to what plant is getting
+	print("send signalsto ui here")
+	
+	if photosyn_count >= current_plant_size && moisture_count >= current_plant_size && nutrients_count >= current_plant_size:
+		return true
+	else:
+		return false
+
+## called by the game manager, needs a signal
 func play_turn():
-	# check 
 	if check_if_plant_is_fully_grown() == false:
 		
 		photosynthesis_vital.current_vital_level -= 1
@@ -54,15 +88,17 @@ func play_turn():
 	if plant_statisfied_round_count == 3:
 		if current_plant_size < final_plant_size:
 			grow_plant()
-	
 
 
 func grow_plant():
-	## send signal to sprites to change
+	current_plant_size += 1
+	## send signal to add new tile / change tile images
+	print("player is grown!")
+	
 	if current_plant_size == final_plant_size:
-		## win!
-		print("player win!")
-		
+		## plant grown success!
+		print("player is grown!")
+
 
 func check_if_plant_tile_has_enough_inputs() -> bool:
 	return false
