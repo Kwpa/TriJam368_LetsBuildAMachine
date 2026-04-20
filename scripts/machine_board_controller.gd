50f2320267eeefea6d6331e79b71eb9bb8393fe8
Kwpa <kai.mcgilligan.oliver@gmail.com>
2026-04-20 12:21:10 -0300

    [Kai] Found new approach for tile collision to test

--- scripts/machine_board_controller.gd
+++ scripts/machine_board_controller.gd
@@ -5,13 +5,16 @@ var selected_tile:Vector2i = Vector2i(-1,-1)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	check_if_tile_is_colliding(1,Vector2i(9,2),Vector2i(9,3))
+	check_if_tile_is_colliding(1,Vector2i(10,2),Vector2i(10,3))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 
 	pass
+
+
 
 func _input(event) -> void:
 	if event is InputEventMouseButton && event.button_index == MOUSE_BUTTON_LEFT && event.pressed:
@@ -31,3 +34,43 @@ func remove_tile() -> void:
 	set_cell(local_to_map(get_global_mouse_position()), -1)
 	print("Tile was removed")
 	# emit a signal so the hand manager can add the card to the hand
+	
+
+func check_if_tile_is_colliding(layer : int, tile1_coords : Vector2i, tile2_coords : Vector2i):
+	
+	var tile1 = get_cell_tile_data(tile1_coords)
+	var tile2 = get_cell_tile_data(tile2_coords)
+	
+	var polys_overlap = false
+	if tile1 != null and tile2 != null:
+		var tile1_poly_count = tile1.get_collision_polygons_count(layer)
+		var tile2_poly_count = tile2.get_collision_polygons_count(layer)
+	
+		if tile1_poly_count > 0 && tile2_poly_count > 0:
+			
+			for i in tile1_poly_count:
+				var polygon_1 : PackedVector2Array = tile1.get_collision_polygon_points(layer,i)
+				print("1")
+				for point : Vector2 in polygon_1:
+					print(point)
+				##	point.x += 0 #tile1_coords.x*128
+				##	point.y += 0 #tile1_coords.y*128
+				print("2")	
+				for j in tile2_poly_count:
+					var polygon_2 : PackedVector2Array = tile2.get_collision_polygon_points(layer,j)
+					for point : Vector2 in polygon_2:
+						print(point)
+					##	point.x += 10000 #tile2_coords.x*128
+					##	point.y += 10000 #tile2_coords.y*128
+					
+					var intersect_array : Array[PackedVector2Array] = Geometry2D.intersect_polygons(polygon_1, polygon_2)
+					if intersect_array.is_empty() == false:
+						print("hurrah!")
+			
+	return polys_overlap
+			
+			
+			
+		 
+	
+	
