91f270c00950196a25d26553f8a07c9520179c66
tristan-remple <tristan.remple@outlook.com>
2026-04-27 04:47:27 -0300

    [tristan] all_cards data structure is filled out

--- scripts/card_data.gd
+++ scripts/card_data.gd
@@ -6,10 +6,11 @@ class_name card_data
 @export var description: String
 @export var coords : Vector2
 @export var resource : Constants.resource
+@export var when_receiving_input : Dictionary
 
-func _init(_title: String, _description: String, _coords: Vector2, _resource: Constants.resource):
+func _init(_title: String, _description: String, _coords: Vector2, _resource: Constants.resource, _receiving_input: Dictionary = {}):
 	title = _title
 	description = _description
 	coords = _coords
 	resource = _resource
-	
+	when_receiving_input = _receiving_input
