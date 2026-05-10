0a9c7a241cb74f3e4e869c9c22fc120ee2f53c87
Kwpa <kai.mcgilligan.oliver@gmail.com>
2026-05-10 10:54:34 -0300

    [Kai] Trying to fix the rotation of tile colliders using alternative tiles

--- scripts/signal_bus.gd
+++ scripts/signal_bus.gd
@@ -3,3 +3,6 @@ extends Node
 signal reset_resource_inputs_on_plant(plant_id)
 signal add_resource_input_to_plant(plant_id) 
 signal spend_action()
+signal enter_remove_mode()
+signal enter_rotate_mode()
+signal enter_hand_mode()
