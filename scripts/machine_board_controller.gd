b9e8f28c4e6b8a33cd22fa2cca6aa9d3a8023374
efraser5005 <estheramfraser@gmail.com>
2026-08-12 09:44:28 -0300

    new signal to fix deactivation bug

--- scripts/machine_board_controller.gd
+++ scripts/machine_board_controller.gd
@@ -26,9 +26,11 @@ var tile_direction := Vector2i(0,-1)
 var applied_transform : int
 
 signal remove_card(card: CardData, pos: Vector2)
+signal clear_dispensers
 
 func load_level(level_def : LevelData):
 	clear()
+	clear_dispensers.emit
 	var plant_count: int = 0
 	for tile in level_def.tiles:
 		print([tile.tilemap_coords, Constants.tile_card_mapping[tile.tile].source_id, Constants.tile_card_mapping[tile.tile].atlas_coords, tile.rotation])
